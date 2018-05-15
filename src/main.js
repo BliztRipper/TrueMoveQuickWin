f385f0b87ec9b6e06c30efc0305cf65145b464f4
Arnon <nonthegenocide@me.com>
2018-05-15 17:15:31 +0700

    finish responsive

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
 //@prepros-prepend ../node_modules/jquery/dist/jquery.js
 //@prepros-prepend ../node_modules/bootstrap/dist/js/bootstrap.bundle.js 
+
+
+$('.more-detail__icon').click( function(e){
+    $(this).toggleClass('expand')
+})
+
+$('.filter__item').click(function (e) {
+    $(this).toggleClass('active');
+})
