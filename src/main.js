4b3505209e0ad04c4d673799b9f17c7909dda297
Arnon <nonthegenocide@me.com>
2018-06-01 14:27:03 +0700

    fix new design

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 //@prepros-prepend ../node_modules/bootstrap/dist/js/bootstrap.bundle.js 
 
 
+
+/*Custom Start From Here*/
 $('.more-detail__icon').click( function(e){
     $(this).toggleClass('expand')
 })
